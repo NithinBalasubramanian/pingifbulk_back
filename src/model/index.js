fce75cc568278b01eb3ffd684ffcfbf95b94ac29
nithin balasubramanian <nihtinfurie17@gmail.com>
2022-11-21 21:46:18 +0530

    setup: mongoDb setup

--- src/model/index.js
+++ src/model/index.js
@@ -5,7 +5,7 @@ const options = {
     useNewUrlParser: true
   };
   
-  const url = "mongodb+srv://nithin:ocean_project@cluster0.dipmxyo.mongodb.net/?retryWrites=true&w=majority";
+  const url = "mongodb+srv://pingifbulk:711015Nith@cluster0.omrh8nh.mongodb.net/test";
   mongoose.connect(url, options);
   
   mongoose.connection.on("connected", function (ref) {
